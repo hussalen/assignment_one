72c4d464a757f967519a6e367cc76e352bf4024b
hussalen <s26136@pjwstk.edu.pl>
2024-04-02 00:43:21 +0200

    testing conflict feature-new

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ class Program
         int sum = 0;
         foreach (int i in ints)
         {
+            Console.WriteLine("Testing conflict");
             sum += i;
         }
         return sum / ints.Length;
