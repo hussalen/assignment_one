3b407a1000127f2c0d3b3d2bdd32b2e65f0ff2cc
hussalen <s26136@pjwstk.edu.pl>
2024-04-02 00:29:56 +0200

    Modification 3

--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 }
