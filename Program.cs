e3eb0083a0def76119f66227782280d97474bc09
hussalen <s26136@pjwstk.edu.pl>
2024-04-02 00:33:11 +0200

    add calculateaverage

--- Program.cs
+++ Program.cs
@@ -9,4 +9,14 @@ class Program
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
     }
+
+    static int CalculateAverage(int[] ints)
+    {
+        int sum = 0;
+        foreach (int i in ints)
+        {
+            sum += i;
+        }
+        return sum / ints.Length;
+    }
 }
